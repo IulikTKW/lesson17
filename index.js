f50026e4e4d8dbfd9b0c5a26f2dd52b84950a969
IulikTKW <inicula1989uk@gmail.com>
2024-10-10 22:46:04 +0300

    1

---
+++ index.js
@@ -0,0 +1,117 @@
+// let x = 10;
+// let y = 99;
+
+// if (1 === 1) {
+//     let y = 5
+//     console.log('1', x)
+//     console.log('2', y)
+//     if (1 === 1) {
+//         let x = 3;
+//         console.log('3', x)
+//         console.log('4', y)      
+
+//     }
+
+
+// }
+
+// console.log('5', x)
+// console.log('6', y)
+
+// var x = 10;
+
+// const testFunc = () => {
+//     var y = 20;
+
+//     console.log('1', x)
+//     console.log('2', y)
+// }
+
+// testFunc()
+
+// console.log('3', x)
+// console.log('4', y)
+
+// testFunc('test', 3)
+
+// const showText = (str, num) => {
+//     if( str.length < num) return console.log('string is too short')
+
+//     let result = '';    
+
+//     for(let i = 0; i < str.length ; i++) {      
+//            result = i < num ? result+=str[i] : result     
+//     }
+
+//     console.log(result)
+// }
+
+// showText('javascript', 5)  
+
+// const arr = ['test', 3, true, {}, [], [ [],[]]]
+
+
+// const arr = [1,2,3,4,5,,,,,,,6]
+
+
+// const arr1 = arr
+
+// arr[0] = 99
+
+
+// console.log(arr.length)
+
+// delete arr[3]
+
+// arr.length = 6
+
+// const arr = [1,2,3,4,5]
+// const arr1 = [...arr, ...arr, ...arr]
+
+// arr1[0] = 99
+
+
+// console.log(arr)
+// console.log(arr1)
+
+// const obj = {
+//     name: 'Alex',
+//     job: 'dev',
+//     city: 'Chisinau'
+// }
+
+// const obj1 = { ...obj, ...obj, name: 'Victor' }
+
+// console.log(obj)
+// console.log(obj1)
+
+// const arr = [1,2,3,4,5]
+
+// arr['indexName'] = 6
+
+// console.log(arr)
+
+// const arr = [1,2,3]
+
+// const arr1 = ['str', 'str', 'str']
+
+// const arr2 = arr.map((item, index) => {   
+//     return item + 1;
+// })
+
+// const addOne = (num) => {
+//     return num + 1;
+// }
+
+// const arr3 = [ addOne(arr[0]), addOne(arr[1]), addOne(arr[2])]
+// const arr4 = [ arr[0] + 1, arr[1] + 1 , arr[2] + 1]
+
+
+// console.log('1', arr2)
+// console.log('2', arr3)
+// console.log('3', arr4)
+
+
+
+
+
